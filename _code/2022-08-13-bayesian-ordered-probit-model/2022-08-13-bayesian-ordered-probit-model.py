724b204e6f7a073a9fe3030779dbc70a6076db8c
Bao Quan <nguyenmbquan95@gmail.com>
2023-02-23 22:22:58 +0000

    Add outline for bayesian ordered probit model.

--- _code/2022-08-13-bayesian-ordered-probit-model/2022-08-13-bayesian-ordered-probit-model.py
+++ _code/2022-08-13-bayesian-ordered-probit-model/2022-08-13-bayesian-ordered-probit-model.py
@@ -1,11 +1,11 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
+#     formats: ipynb,py:percent
 #     notebook_metadata_filter: title,author,layout,use_math
 #     text_representation:
 #       extension: .py
-#       format_name: light
+#       format_name: percent
 #       format_version: '1.5'
 #       jupytext_version: 1.14.1
 #   kernelspec:
@@ -17,5 +17,24 @@
 #   use_math: true
 # ---
 
+# %% [markdown]
 # # Ordinal Data in Real World
-# #
+
+# %% [markdown]
+# # Modelling
+
+# %% [markdown]
+# # Implementation in Numpyro
+# ## Data
+# ## Model
+# ## Results
+
+# %% [markdown]
+# # Conclusions
+
+# %% [markdown]
+# # References
+# - Chapter 23, Doing Bayesian Data Analysis, 2nd Edition, John K. Kruschke,
+# https://sites.google.com/site/doingbayesiandataanalysis/
+# - https://betanalpha.github.io/assets/case_studies/ordinal_regression.html
+# - https://num.pyro.ai/en/stable/tutorials/ordinal_regression.html
