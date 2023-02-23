7fa2196039c9432531255df20bf7bfabf3df25bd
Bao Quan <nguyenmbquan95@gmail.com>
2023-02-23 21:27:08 +0000

    Rename bayesian ordered probit model post

---
+++ _code/2022-08-13-bayesian-ordered-probit-model/2022-08-13-bayesian-ordered-probit-model.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     notebook_metadata_filter: title,author,layout,use_math
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+#   layout: post
+#   title: Bayesian Ordered Probit Model
+#   use_math: true
+# ---
+
+# # Ordinal Data in Real World
+# #
