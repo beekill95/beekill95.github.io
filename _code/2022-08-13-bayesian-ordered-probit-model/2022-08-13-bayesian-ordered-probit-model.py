6c58e60a45e1d0a7a585ca777346d3559b3d3d85
Bao Quan <nguyenmbquan95@gmail.com>
2023-02-26 12:32:06 +0000

    Finish oridinal data introduction.

--- _code/2022-08-13-bayesian-ordered-probit-model/2022-08-13-bayesian-ordered-probit-model.py
+++ _code/2022-08-13-bayesian-ordered-probit-model/2022-08-13-bayesian-ordered-probit-model.py
@@ -25,17 +25,33 @@
 # However, unlike metric data type,
 # where we can easily calculate the distance between variables,
 # we couldn't do the same with ordinal variables.
-#
+
 # There are many examples of ordinal data type in real world.
 # For instance, in many e-commerce websites such as Amazon, eBay, etc.,
 # ordinal data appeared as ratings given to a product by customers.
 # Or in schools or educational institutes,
 # letter grades given by teachers to evaluate the performance of students
 # on exams or tests can also be considered as ordinal data.
-#
+# Or if we ask a bunch of people filling in a survey form by expressing
+# their aggreement on a scale from 1 to 7
+# (with 1 being "totally disagree" and 7 being "totally agree")
+# about various statements.
 
-# Talk about ordinal data, but using 1, 2, 3
-# Talk about what we want to uncover with ordinal data type.
+# It is also important to note that in the examples above,
+# even though the labels of the categories are numbers,
+# they are not metric variables.
+# Thus, these numbers should be thought as text labels,
+# representing an ascending or descending order.
+# In fact, many research papers model oridinal data as metric data
+# and get wrong results (TODO: citation here).
+
+# Naturally, we would want to ask how customers, teachers, subjects come up
+# with such ratings, evaluations, and answers, respectively.
+# Moreover, we would want compare between products or students
+# to find out which product or student is the best.
+# Or in the case of the survey form,
+# we want to find which statements are widely believed,
+# or which statements are controversial.
 
 # %% [markdown]
 # # Modelling
